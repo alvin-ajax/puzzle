3b526f97781cc0c9616966286f286344d1b7fd01
alvin-ajax <alvinmok@aol.com>
2023-07-22 19:46:45 +0800

    First commit

---
+++ precache-manifest.c6a74fff0372e8f465c1a66078ef7145.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7836af93a49a6e10c53d97db12d0955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2680b489f4efe0ddecdf",
+    "url": "/static/css/main.5675d3c9.chunk.css"
+  },
+  {
+    "revision": "5274b65381026cd00201",
+    "url": "/static/js/2.466ad9b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.466ad9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2680b489f4efe0ddecdf",
+    "url": "/static/js/main.b854309f.chunk.js"
+  },
+  {
+    "revision": "50e84d881e5ab2abeb1d",
+    "url": "/static/js/runtime-main.7f8a1c98.js"
+  }
+]);
